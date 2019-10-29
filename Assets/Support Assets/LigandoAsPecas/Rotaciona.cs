b5e48fe81f41f673481cdb5187456a48763aa58b
atdayan <arthur.dayan31@gmail.com>
2019-10-29 05:14:30 -0300

    Added new Materials/Pipes/Support Assets

---
+++ Assets/Support Assets/LigandoAsPecas/Rotaciona.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotaciona : MonoBehaviour {
+
+    public float turnSpeed = 1f;
+    bool rotate = false;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rotate){
+            transform.Rotate(Vector3.forward, -turnSpeed);
+            int angulo = (int)Mathf.Round(transform.rotation.eulerAngles.z);
+            //print(angulo);
+            if (angulo == 0 || angulo == 270 || angulo == 90 || angulo == 180){
+                rotate = false;
+            }
+        }
+    }
+
+    void OnMouseDown(){
+        rotate = true;
+    }
+}
